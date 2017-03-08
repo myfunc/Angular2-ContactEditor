ab94e0fcdbf2ec1ab867e8bf1b76951cacf55e96
myfunc <de5171245@gmail.com>
2017-03-08 20:43:05 +0200

    Update clocks.component.ts

--- app/clocks/clocks.component.ts
+++ app/clocks/clocks.component.ts
@@ -112,11 +112,10 @@ export class ClocksComponent implements OnInit, OnDestroy{
         this.context.moveTo(this.canvas.width / 2, this.canvas.height / 2);
         this.context.lineTo((this.canvas.width / 2 + Math.cos(this.angle) * this.clockDivisions),
             this.canvas.height / 2 + Math.sin(this.angle) * this.clockDivisions);
-        // DRAW THE TAIL OF THE SECONDS HAND.
         this.context.moveTo(this.canvas.width / 2, this.canvas.height / 2);
         this.context.lineTo((this.canvas.width / 2 - Math.cos(this.angle) * 20),
             this.canvas.height / 2 - Math.sin(this.angle) * 20);
-        this.context.strokeStyle = '#586A73';
+        this.context.strokeStyle = '#555555';
         this.context.stroke();
 
     }
@@ -129,7 +128,7 @@ export class ClocksComponent implements OnInit, OnDestroy{
         this.context.moveTo(this.canvas.width / 2, this.canvas.height / 2);
         this.context.lineTo((this.canvas.width / 2 + Math.cos(this.angle) * this.clockDivisions / 1.1),
             this.canvas.height / 2 + Math.sin(this.angle) * this.clockDivisions / 1.1);
-        this.context.strokeStyle = '#999';
+        this.context.strokeStyle = '#999999';
         this.context.stroke();
     }
     canvasDrawHourArrow() {
